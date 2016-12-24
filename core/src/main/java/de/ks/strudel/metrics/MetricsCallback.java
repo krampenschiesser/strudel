7f9afd3e3e710409770d7f6194bb88259abbedc5
krampenschiesser <krampenschiesser@gmail.com>
2016-12-24 10:44:30 +0100

    added chapter about metrics

--- core/src/main/java/de/ks/strudel/metrics/MetricsCallback.java
+++ core/src/main/java/de/ks/strudel/metrics/MetricsCallback.java
@@ -19,18 +19,56 @@ import de.ks.strudel.route.HttpMethod;
 import de.ks.strudel.route.Route;
 import io.undertow.server.HttpServerExchange;
 
+/**
+ * Interface used for metrics.
+ * Once an instance is bound to this interface metrics will be automatically collected.
+ */
 public interface MetricsCallback {
+  /**
+   * Tracks exceptions beeing thrown by handlers
+   *
+   * @param exchange e
+   * @param e        e
+   */
   void trackException(HttpServerExchange exchange, Exception e);
 
+  /**
+   * Tracks a call of a route(before executing the handler)
+   * @param exchange e
+   * @param route r
+   */
   void trackRouteExecuted(HttpServerExchange exchange, Route route);
 
+  /**
+   * Tracks execution time of a route. Use this to detect slow handlers
+   * @param exchange e
+   * @param route r
+   * @param timeInNs nanosecond
+   */
   void trackRouteExecutionTime(HttpServerExchange exchange, Route route, long timeInNs);
 
+  /**
+   * For counting how many synchronous routes were called
+   */
   void trackSyncRouteCall();
 
+  /**
+   * For counting how many asynchronous routes were called
+   */
   void trackAsyncRouteCall();
 
+  /**
+   * Tracks every exchange that is passed into the root handler
+   * @param exchange e
+   */
   void trackExchange(HttpServerExchange exchange);
 
+  /**
+   * Tracks calls to routes that are not mapped.
+   *
+   * @param exchange e
+   * @param method http method of the call
+   * @param url of the call
+   */
   void trackUnknownRoute(HttpServerExchange exchange, HttpMethod method, String url);
 }
