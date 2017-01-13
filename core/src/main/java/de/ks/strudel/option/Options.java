ba96ca65e5bc1d72fd21f2ca6405fad3150453fa
krampenschiesser <krampenschiesser@gmail.com>
2017-01-13 18:59:51 +0100

    strudel https support

--- core/src/main/java/de/ks/strudel/option/Options.java
+++ core/src/main/java/de/ks/strudel/option/Options.java
@@ -1,12 +1,12 @@
 /**
  * Copyright [2016] [Christian Loehnert]
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,12 +15,35 @@
  */
 package de.ks.strudel.option;
 
+import com.google.inject.Inject;
+
+import javax.annotation.Nullable;
 import javax.inject.Singleton;
+import javax.net.ssl.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Singleton
 public class Options {
   private int port = 4567;
   private String host = "0.0.0.0";
+  private SSLContext sslContext;
+
+  /**
+   * Injection point for own ssl context
+   *
+   * @param sslContext created by you guice module
+   * @return self for chaining
+   */
+  @Inject(optional = true)
+  public Options setSslContext(SSLContext sslContext) {
+    this.sslContext = sslContext;
+    return this;
+  }
 
   /**
    * @return the configured host adress
@@ -50,7 +73,83 @@ public class Options {
     return port;
   }
 
-//  public Options secure(String keystoreFile, String keystorePassword, String truststoreFile, String truststorePassword) {
-//    return this;
-//  }
+  /**
+   * Constructs a new ssl context for starting undertow https server.
+   * You can also provide the ssl context yourself in a guice module which is injected via {@link #setSslContext(SSLContext)}
+   *
+   * @param keystoreFile     nullable file, either classpath or absolute
+   * @param keystorePassword password for the keystore
+   * @return self for chaining
+   * @throws IOException               e
+   * @throws KeyStoreException         e
+   * @throws NoSuchAlgorithmException  e
+   * @throws CertificateException      e
+   * @throws UnrecoverableKeyException e
+   * @throws KeyManagementException    e
+   */
+
+  public Options secure(String keystoreFile, String keystorePassword) throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, IOException, KeyManagementException, KeyStoreException {
+    return secure(keystoreFile, keystorePassword, null, null);
+  }
+
+  /**
+   * Constructs a new ssl context for starting undertow https server.
+   * You can also provide the ssl context yourself in a guice module which is injected via {@link #setSslContext(SSLContext)}
+   *
+   * @param keystoreFile       nullable file, either classpath or absolute
+   * @param keystorePassword   password for the keystore
+   * @param truststoreFile     nullable file, either classpath or absolute
+   * @param truststorePassword password for the keystore
+   * @return self for chaining
+   * @throws IOException               e
+   * @throws KeyStoreException         e
+   * @throws NoSuchAlgorithmException  e
+   * @throws CertificateException      e
+   * @throws UnrecoverableKeyException e
+   * @throws KeyManagementException    e
+   */
+  public Options secure(String keystoreFile, String keystorePassword, @Nullable String truststoreFile, @Nullable String truststorePassword) throws IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException, KeyManagementException {
+    KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+    keyManagerFactory.init(loadKeyStore(keystoreFile, keystorePassword), keystorePassword.toCharArray());
+    KeyManager[] keyManagers = keyManagerFactory.getKeyManagers();
+
+    TrustManager[] trustManagers = new TrustManager[0];
+    if (truststoreFile != null && truststorePassword != null) {
+      TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+      trustManagerFactory.init(loadKeyStore(truststoreFile, truststorePassword));
+      trustManagers = trustManagerFactory.getTrustManagers();
+    }
+
+    SSLContext sslContext = SSLContext.getInstance("TLS");
+    sslContext.init(keyManagers, trustManagers, new SecureRandom());
+    this.sslContext = sslContext;
+    return this;
+  }
+
+  private KeyStore loadKeyStore(String keystoreFile, String keystorePassword) throws IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
+    InputStream keyStoreStream = null;
+    try {
+      URL resource = getClass().getResource(keystoreFile);
+      if (resource != null) {
+        keyStoreStream = resource.openStream();
+      } else {
+        keyStoreStream = new FileInputStream(keystoreFile);
+      }
+      KeyStore keyStore = KeyStore.getInstance("JKS");
+      keyStore.load(keyStoreStream, keystorePassword.toCharArray());
+      return keyStore;
+    } finally {
+      if (keyStoreStream != null) {
+        keyStoreStream.close();
+      }
+    }
+  }
+
+  public boolean isSecure() {
+    return sslContext != null;
+  }
+
+  public SSLContext getSslContext() {
+    return sslContext;
+  }
 }
--- core/src/main/java/de/ks/strudel/server/ServerManager.java
+++ core/src/main/java/de/ks/strudel/server/ServerManager.java
@@ -1,12 +1,12 @@
 /**
  * Copyright [2016] [Christian Loehnert]
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,7 +33,8 @@ public class ServerManager {
 
   final AtomicReference<Undertow> undertowRef = new AtomicReference<>();
 
-  @Inject public ServerManager(Options options, Router router) {
+  @Inject
+  public ServerManager(Options options, Router router) {
     this.options = options;
     this.router = router;
   }
@@ -47,10 +48,14 @@ public class ServerManager {
       int port = options.port();
       String host = options.host();
 
-      Undertow undertow = Undertow.builder()//
-                                  .addHttpListener(port, host)//
-                                  .setHandler(router.getHandler())//
-                                  .build();
+      Undertow.Builder builder = Undertow.builder();
+      if (options.isSecure()) {
+        builder.addHttpsListener(port, host, options.getSslContext());
+      } else {
+        builder.addHttpListener(port, host);
+      }
+      builder.setHandler(router.getHandler());
+      Undertow undertow = builder.build();
       undertow.start();
       undertowRef.compareAndSet(null, undertow);
     }
